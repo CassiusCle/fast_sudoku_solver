dbe015fd5916d8d4afbbd50c84e6819fcfcb2e54
CassiusCle <clemenshendr@gmail.com>
2024-05-09 15:48:00 +0200

    Refactor techniques

--- sudoku_solver/techniques.py
+++ sudoku_solver/techniques.py
@@ -1,6 +1,6 @@
 """TODO: Module docstring"""
 
-from typing import Tuple
+from typing import Tuple, Set
 
 import numpy as np
 
@@ -16,8 +16,8 @@ def apply_constraint_propagation(
     cell in the puzzle until the puzzle is solved or no further progress can be made.
 
     Args:
-        puzzle_2d: A 2D NumPy array representing the current state of the Sudoku puzzle.
-                   Each cell contains the value of the puzzle (1-9) or 0 if the value is unknown.
+        puzzle_2d:  A 2D NumPy array representing the current state of the Sudoku puzzle.
+                    Each cell contains the value of the puzzle (1-9) or 0 if the value is unknown.
         options_3d: A 3D NumPy array representing the possible values for each cell.
                     The first two dimensions correspond to the puzzle grid, and the third dimension
                     contains a binary indicator for the possible values (1-9).
@@ -38,7 +38,7 @@ def apply_constraint_propagation(
     if not (isinstance(options_3d, np.ndarray) and options_3d.ndim == 3):
         raise ValueError("options_3d must be a 3D NumPy array.")
 
-    iteration_count = 0
+    iteration_count: int = 0
     while True:
         iteration_count += 1
         # Apply elimination technique
@@ -47,7 +47,8 @@ def apply_constraint_propagation(
         )
         if is_solved:
             break
-        elif not has_progress and iteration_count > 1:
+
+        if not has_progress and iteration_count > 1:
             break
 
         # Apply hidden singles technique
@@ -84,12 +85,12 @@ def _apply_elimination(
         - puzzle_2d: The updated 2D puzzle state.
         - options_3d: The updated 3D options cube.
     """
+    is_solved: bool = False
 
-    i = 0
-    has_progress = False
-    is_solved = False
-
+    iteration_count: int = 0
     while True:
+        iteration_count += 1
+
         # Store the previous state of the puzzle to detect changes
         prev_puzzle_2d = puzzle_2d
         # prev_options_3d = options_3d.copy()
@@ -100,61 +101,159 @@ def _apply_elimination(
         rows, cols = known_cells[:, 0], known_cells[:, 1]
         values = puzzle_2d[rows, cols] - 1
 
-        # Eliminate options based on known cell values
-        options_3d[rows, :, values] = 0
-        options_3d[:, cols, values] = 0
-        box_start_rows, box_start_cols = 3 * (rows // 3), 3 * (cols // 3)
-        for box_start_row, box_start_col, value in zip(
-            box_start_rows, box_start_cols, values
-        ):
-            options_3d[
-                box_start_row : box_start_row + 3,
-                box_start_col : box_start_col + 3,
-                value,
-            ] = 0
+        # Update puzzle based on known values
+        puzzle_2d, options_3d = _update_puzzle(rows, cols, values, options_3d)
 
-        # Set known cells back to one
-        options_3d[rows, cols, values] = 1
-
-        # Update the puzzle state based on the options cube
-        puzzle_2d = options_3d.argmax(axis=2) + 1
-        # Reset cells with multiple options to zero
-        puzzle_2d[options_3d.sum(axis=2) != 1] = 0
-
-        i += 1
-
-        # Check for changes in the puzzle state to determine progress
-        # if np.array_equal(options_3d, prev_options_3d): # TODO: Change to 3D?  (better flow then with "solved")
-        if np.array_equal(
-            puzzle_2d, prev_puzzle_2d
-        ):  # TODO: Change to 3D? (better flow then with "solved")
-            if i > 1:
-                has_progress = True
-            else:
-                has_progress = False
+        # Check for changes in the puzzle state and break out if no progress was made this iteration
+        has_progress = not np.array_equal(puzzle_2d, prev_puzzle_2d)
+        if not has_progress:
+            has_progress = iteration_count > 1
             break
 
-        # Check if the puzzle is solved
-        if puzzle_2d.sum() == 405:
-            is_solved = True
+        # Check if the puzzle is solved and breakout if it is
+        is_solved = np.all(puzzle_2d > 0)
+        if is_solved:
             break
 
     return has_progress, is_solved, puzzle_2d, options_3d
+
+
+def _find_singles(options_3d: np.ndarray) -> Set[Tuple[int, int, int]]:
+    """
+    Find all unique options for digits in rows, columns, or subsquares of a Sudoku puzzle.
+
+    This function identifies all cells within the options cube that represent the only possible
+    choice for a digit in its respective row, column, or 3x3 subsquare. It is a part of the
+    constraint propagation process in solving a Sudoku puzzle.
+
+    Args:
+        options_3d: A 3D NumPy array representing the possible values for each cell in the Sudoku puzzle.
+                    The first two dimensions correspond to the puzzle grid, and the third dimension
+                    contains a binary indicator for the possible values (1-9).
+
+    Returns:
+        A set of tuples, where each tuple contains the row index, column index, and value index
+        of a cell that represents a unique option for that digit in its row, column, or subsquare.
+
+    """
+    singles = set()
+
+    # Compute singles in columns
+    cols_w_singles = np.argwhere(options_3d.sum(axis=0) == 1)
+    row_indices = options_3d.argmax(axis=0)
+    singles.update({(row_indices[c, v], c, v) for c, v in cols_w_singles})
+
+    # Compute singles in rows
+    rows_w_singles = np.argwhere(options_3d.sum(axis=1) == 1)
+    col_indices = options_3d.argmax(axis=1)
+    singles.update({(r, col_indices[r, v], v) for r, v in rows_w_singles})
+
+    # Compute singles in 3x3 subsquares
+    for x in range(0, 9, 3):
+        for y in range(0, 9, 3):
+            subsquare = options_3d[y:y+3, x:x+3, :]
+            row_idx = np.argmax(subsquare.sum(axis=1), axis=0)
+            col_idx = np.argmax(subsquare.sum(axis=0), axis=0)
+            depth_idx = np.argwhere(subsquare.sum(axis=(0, 1)) == 1)
+
+            singles.update({
+                (r[0] + y, c[0] + x, v)
+                for r, c, v in zip(row_idx[depth_idx], col_idx[depth_idx], depth_idx.flatten())
+            })
+
+    return singles
+
+def _compute_hidden_singles(
+    singles: Set[Tuple[int, int, int]], puzzle_2d: np.ndarray
+) -> Set[Tuple[int, int, int]]:
+    """
+    Compute the hidden singles for a Sudoku puzzle.
+
+    A hidden single occurs when a cell is the only one in a row, column, or block
+    that can accommodate a certain number. This function calculates the hidden singles
+    by subtracting the known values from the set of all singles.
+
+    Args:
+        singles:    A set of tuples representing cells that are the only option for a digit
+                    in their row, column, or subsquare.
+        puzzle_2d:  A 2D NumPy array representing the current state of the Sudoku puzzle.
+
+    Returns:
+        A set of tuples representing the hidden singles in the puzzle.
+
+    """
+    # Compute already known values
+    known_values = {(*kv, puzzle_2d[*kv] - 1) for kv in np.argwhere(puzzle_2d > 0)}
+
+    # Return the hidden singles
+    return singles - known_values
+
+def _update_puzzle(
+    rows: np.array, cols: np.array, values: np.array, options_3d: np.ndarray
+) -> np.ndarray:
+    """
+    Update the puzzle state based on the known values and options cube.
+
+    This function updates the options cube by setting the possibilities for known values
+    to zero in their respective rows, columns, and blocks. It then updates the puzzle state
+    based on the options cube, setting cells with multiple options back to zero.
+
+    Args:
+        rows: A NumPy array of row indices for known values.
+        cols: A NumPy array of column indices for known values.
+        values: A NumPy array of values corresponding to the indices in rows and cols.
+        options_3d: A 3D NumPy array representing the possible values for each cell.
+
+    Returns:
+        A tuple containing the updated 2D puzzle state and the updated 3D options cube.
+
+    """
+    # Set column, row, and block to zero for all known cells
+    options_3d[rows, :, values] = 0
+    options_3d[:, cols, values] = 0
+    options_3d[rows, cols, :] = 0
+
+    # Calculate the starting indices of the 3x3 blocks
+    block_start_rows = 3 * (rows // 3)
+    block_start_cols = 3 * (cols // 3)
+
+    # Set the possibilities in the 3x3 blocks to zero
+    for block_start_row, block_start_col, value in zip(
+        block_start_rows, block_start_cols, values
+    ):
+        options_3d[
+            block_start_row : block_start_row + 3,
+            block_start_col : block_start_col + 3,
+            value,
+        ] = 0
+
+    # Set known cells back to one
+    options_3d[rows, cols, values] = 1
+
+    # Update the puzzle state based on the options cube
+    puzzle_2d = options_3d.argmax(axis=2) + 1
+    # Reset cells with multiple options to zero
+    puzzle_2d[options_3d.sum(axis=2) != 1] = 0
+
+    return puzzle_2d, options_3d
 
 
 def _apply_hidden_singles(
     puzzle_2d: np.ndarray, options_3d: np.ndarray
 ) -> Tuple[bool, bool, np.ndarray, np.ndarray]:
     """
-    Apply the 'hidden singles' rule to the Sudoku puzzle until no further progress is made.
+    Apply the 'hidden singles' rule to the Sudoku puzzle.
 
-    # This method iteratively applies Sudoku elimination rules to the given puzzle.
-    # It updates the puzzle state and the options cube until the puzzle is solved
-    # or no more progress can be made.
+    This function identifies 'hidden singles' in the puzzle, which are cells that are the only ones
+    in a row, column, or block that can accommodate a certain number. It updates the puzzle state
+    and options cube accordingly.
 
     Args:
-        puzzle_2d: A 2D NumPy array representing the current state of the Sudoku puzzle.
+        puzzle_2d:  A 2D NumPy array representing the current state of the Sudoku puzzle.
+                    Each cell contains the value of the puzzle (1-9) or 0 if the value is unknown.
         options_3d: A 3D NumPy array representing the possible values for each cell.
+                    The first two dimensions correspond to the puzzle grid, and the third dimension
+                    contains a binary indicator for the possible values (1-9).
 
     Returns:
         A tuple containing:
@@ -162,95 +261,19 @@ def _apply_hidden_singles(
         - is_solved: A boolean indicating if the puzzle is solved.
         - puzzle_2d: The updated 2D puzzle state.
         - options_3d: The updated 3D options cube.
+
     """
-    # Store the previous state of the puzzle to detect changes
-    # prev_options_3d = options_3d.copy()
-    prev_puzzle_2d = puzzle_2d
+    prev_puzzle_2d = puzzle_2d.copy()
+    singles = _find_singles(options_3d)
+    hidden_singles = _compute_hidden_singles(singles, puzzle_2d)
 
-    # Compute columns with singles
-    cols_w_singles = np.argwhere((options_3d.sum(axis=0) == 1))  # 3d
-    # Compute the row on which the single is found
-    row_indices = options_3d.argmax(axis=0)
-    # Singles from columns
-    singles = {(row_indices[c, v], c, v) for c, v in cols_w_singles}
-
-    # Compute rows with singles
-    rows_w_singles = np.argwhere((options_3d.sum(axis=1) == 1))  # 3d
-    # Compute the colum on which the single is found
-    col_indices = options_3d.argmax(axis=1)
-    # Singles from rows
-    singles.update({(r, col_indices[r, v], v) for r, v in rows_w_singles})
-
-    for x in range(0, 9, 3):
-        for y in range(0, 9, 3):
-            # Compute row on which single is found
-            row_idx = np.argmax(options_3d[y : y + 3, x : x + 3, :].sum(axis=1), axis=0)
-
-            # Compute column on which single is found
-            column_agg = options_3d[y : y + 3, x : x + 3, :].sum(axis=0)
-            col_idx = np.argmax(column_agg, axis=0)
-
-            # Compute depth on which single is found
-            depth_idx = np.argwhere(column_agg.sum(axis=0) == 1)
-
-            # Singles from this subsquare
-            singles.update(
-                {
-                    (r[0] + y, c[0] + x, v[0])
-                    for r, c, v in zip(
-                        row_idx[depth_idx], col_idx[depth_idx], depth_idx
-                    )
-                }
-            )
-
-    # Compute already known values
-    known_values = {(*kv, puzzle_2d[*kv] - 1) for kv in np.argwhere(puzzle_2d)}
-
-    # Compute hidden singles
-    hidden_singles: set = singles - known_values
-
-    # Return in case there are no hidden singles
     if not hidden_singles:
-        has_progress = False
-        is_solved = False
-        return has_progress, is_solved, puzzle_2d, options_3d
+        return False, False, puzzle_2d, options_3d
 
-    rows, cols, values = zip(*hidden_singles)
-    rows = np.array(rows)
-    cols = np.array(cols)
-    values = np.array(values)
+    # Update puzzle using newly discovered known values
+    puzzle_2d, options_3d = _update_puzzle(*map(np.array, zip(*hidden_singles)), options_3d)
 
-    # Set column, row, and box to zero for all known cells
-    options_3d[rows, :, values] = 0
-    options_3d[:, cols, values] = 0
-    options_3d[rows, cols, :] = 0
-    box_start_rows, box_start_cols = 3 * (rows // 3), 3 * (cols // 3)
-    for box_start_row, box_start_col, value in zip(
-        box_start_rows, box_start_cols, values
-    ):
-        options_3d[
-            box_start_row : box_start_row + 3, box_start_col : box_start_col + 3, value
-        ] = 0
-
-    # Set known cells back to one
-    options_3d[rows, cols, values] = 1
-
-    puzzle_2d = options_3d.argmax(axis=2) + 1
-    puzzle_2d[options_3d.sum(axis=2) != 1] = 0
-
-    # Check for changes in the puzzle state to determine progress
-    # if np.array_equal(options_3d, prev_options_3d): # TODO: Change to 3D?  (better flow then with "solved")
-    if np.array_equal(
-        puzzle_2d, prev_puzzle_2d
-    ):  # TODO: Change to 3D?  (better flow then with "solved")
-        has_progress = False
-    else:
-        has_progress = True
-
-    # Check if the puzzle is solved
-    if puzzle_2d.sum() == 405:
-        is_solved = True
-    else:
-        is_solved = False
+    has_progress: bool = not np.array_equal(puzzle_2d, prev_puzzle_2d)
+    is_solved: bool = np.all(puzzle_2d > 0)
 
     return has_progress, is_solved, puzzle_2d, options_3d
